4a19a7f39dc9ac8502aa03c746e9ab5b347f822d
zaleksandrov <138000351+zaleksandrov@users.noreply.github.com>
2023-06-28 12:51:35 +0300

    Create Example.cs

---
+++ Example.cs
@@ -0,0 +1,14 @@
+include UnityEngine;
+
+public class Example : MonoBehaviour
+{
+
+  public int Add(int x, int y) => x + y;
+
+  public int Sub(int x, int y) => x - y;
+
+  public int Mult(int x, int y) => x * y;
+
+  public int Div(int x, int y) => x / y;
+
+}
